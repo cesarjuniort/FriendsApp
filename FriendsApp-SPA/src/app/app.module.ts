16f65cee75c9648ab759865558851818a83bf350
Cesar Toribio <cesarjuniort@gmail.com>
2019-04-13 23:25:52 -0400

    ftr-web-10 - Added the photo editor component.

--- FriendsApp-SPA/src/app/app.module.ts
+++ FriendsApp-SPA/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MemberListResolver } from './resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './resolvers/member-edit.resolver';
 import { PreventUnsaveChanges } from './guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 
 export function tokenGetter() {
@@ -43,7 +44,8 @@ export function tokenGetter() {
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
-    MemberEditComponent
+    MemberEditComponent,
+    PhotoEditorComponent
   ],
   imports: [
     BrowserModule,
