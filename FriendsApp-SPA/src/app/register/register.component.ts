7e5dcbbed67885f9f0793f2fb76dc6815814fd86
Cesar Toribio <cesarjuniort@gmail.com>
2019-04-16 21:54:43 -0400

    ftr-web-11 - Converted the register component into a ReactiveForm; added new fields at user registration; included the BsDatepicker for a consistent date input across browsers.

--- FriendsApp-SPA/src/app/register/register.component.ts
+++ FriendsApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -10,20 +12,57 @@ import { AlertifyService } from '../services/alertify.service';
 export class RegisterComponent implements OnInit {
   model: any = {};
 
+  registerForm: FormGroup;
+
+  bsCalendarConfig: Partial<BsDatepickerConfig>;
+
   @Output()
   cancelRegistration = new EventEmitter();
 
   constructor(private authService: AuthService,
-    private alertify: AlertifyService) { }
+    private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.bsCalendarConfig = {
+      containerClass:'theme-red'
+    };
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      confirmPassword: ['', [Validators.required,Validators.minLength(5)]],
+      gender: ['male'],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required]
+    }, {validator:this.passwordValidationMatch});
+    
+    
+  }
+
+  passwordValidationMatch(f: FormGroup){
+    if (f.get('password').value === f.get('confirmPassword').value)
+      return null;
+    else return {'mismatch':true};
+  }
+
+  isControlInvalid(name:string, forError?:string){
+    const ctrl = this.registerForm.get(name);
+    if(!ctrl)
+      return false;
+
+    if(forError){
+      return ctrl.hasError(forError) && ctrl.touched;
+    } 
+    return ctrl.errors && ctrl.touched;
   }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      (next) => { this.alertify.success('Registration successful!'); },
-      (error) => { this.alertify.error(error); }
-    );
+    console.log(this.registerForm.value);
+    // this.authService.register(this.model).subscribe(
+    //   (next) => { this.alertify.success('Registration successful!'); },
+    //   (error) => { this.alertify.error(error); }
+    // );
   }
 
   cancel() {
