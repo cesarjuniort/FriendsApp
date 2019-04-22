66586372718262e5f4cc557a8b5de4abfc0d8c52
Cesar Toribio <cesarjuniort@gmail.com>
2019-04-22 11:39:17 -0400

    ftr-web-13 - Added basic sorting capabilities.

--- FriendsApp.API/Data/FriendsRepository.cs
+++ FriendsApp.API/Data/FriendsRepository.cs
@@ -34,12 +34,25 @@ namespace FriendsApp.API.Data
 
         public async Task<PagedList<User>> GetUsers(PageRequestUserParams pageInfo)
         {
-            var users = context.Users.Include(p=>p.Photos).AsQueryable();
+            var users = context.Users.Include(p=>p.Photos).OrderByDescending(u=>u.LastActive).AsQueryable();
             users = users.Where(u => u.Id != pageInfo.UserId);
             users = users.Where(u => u.Gender == pageInfo.Gender); // that could be combined with previous line, but wanted to keep it separated to remind that 'where' predicates can be combined in chunks, very powerful stuff.
             var minDBO = DateTime.Today.AddYears(-pageInfo.MinAge);
             var maxDBO = DateTime.Today.AddYears(-pageInfo.MaxAge);
             users = users.Where(u => u.DateOfBirth<= minDBO && u.DateOfBirth >= maxDBO);
+
+            if(!string.IsNullOrEmpty(pageInfo.OrderBy)) 
+            {
+                switch (pageInfo.OrderBy) {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
+            }
+
             return await PagedList<User>.CreateAsync(users,pageInfo.PageNumber,pageInfo.PageSize);
         }
 
--- FriendsApp.API/Helpers/PageRequestUserParams.cs
+++ FriendsApp.API/Helpers/PageRequestUserParams.cs
@@ -17,6 +17,9 @@ namespace FriendsApp.API.Helpers
         public int MinAge {get;set;}
         public int MaxAge {get;set;}
 
+        public string OrderBy {get;set;}
+
+
         
     }
 }
