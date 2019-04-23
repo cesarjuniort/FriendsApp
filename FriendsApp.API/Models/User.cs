3ac6a238bb0a8fadad11461198e3e2aeeb49d8da
Cesar Toribio <cesarjuniort@gmail.com>
2019-04-22 23:46:41 -0400

    ftr-web-14 - Added the Likes table and defined the EF Migrations that will establish the many-to-many relationship between the Liker and the Likee.

--- FriendsApp.API/Models/User.cs
+++ FriendsApp.API/Models/User.cs
@@ -23,6 +23,8 @@ namespace FriendsApp.API.Models
         public string Country { get; set; }
         public string Interests { get; set; }
         public ICollection<Photo> Photos {get;set;}
+        public ICollection<Like> Likers {get;set;}
+        public ICollection<Like> Likees {get;set;}
 
 
     }
